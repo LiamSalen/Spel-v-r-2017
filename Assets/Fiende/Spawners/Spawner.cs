e0ba87d14b13423fc07db73d466a791d601e043a
Liam Morgan <Liamsipad@outlook.com>
2017-02-20 11:50:30 +0100

    20/02/17 11:50

---
+++ Assets/Fiende/Spawners/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    public GameObject enemy;
+    public Vector2 position;
+    public float timer;
+    private float startTimer;
+	// Use this for initialization
+	void Start () {
+        startTimer = timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (timer <= 0)
+        {
+            Instantiate(enemy, position, Quaternion.identity);
+            timer = startTimer;
+        }
+        timer -= Time.deltaTime;
+	}
+}
