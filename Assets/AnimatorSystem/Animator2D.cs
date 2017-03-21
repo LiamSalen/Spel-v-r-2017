482fd1cebdac6ed25ecf1d8d7fd659ba630b6123
Liam Morgan <Liamsipad@outlook.com>
2017-03-21 09:05:36 +0100

    20/03/17 16:00

---
+++ Assets/AnimatorSystem/Animator2D.cs
@@ -0,0 +1,258 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public delegate void CallBack ();
+
+[System.Serializable]
+public class Animation {
+
+	public List<Sprite> frames;
+	public int frameRate;
+	[HideInInspector]
+	public int currentFrame;
+	public string name = "No Name";
+	public bool looping;
+
+	private Dictionary <int, CallBack> callBacks = new Dictionary <int, CallBack>();
+
+	public Animation () {
+		frames = new List<Sprite> ();
+		frameRate = 24;
+		currentFrame = 0;
+		name = "No Name";
+	}
+
+	public void Reset () {
+		currentFrame = 0;
+	}
+
+	public bool ReachedEnd () {
+		if (currentFrame == frames.Count - 1)
+			return true;
+		else
+			return false;
+	}
+
+	public void Increment () {
+		currentFrame++;
+		if (currentFrame >= frames.Count)
+		{
+			if (looping)
+			{
+				currentFrame = 0;
+			}
+			else
+			{
+				currentFrame = frames.Count-1;
+			}
+		}
+	
+		if (callBacks.Count > 0 && callBacks.ContainsKey (currentFrame))
+		{
+			CallBack call = callBacks [currentFrame];
+			if (call != null)
+			{
+				call ();
+			}
+		}
+	}
+
+	public Sprite GetFrame () {
+		return frames [currentFrame];
+	}
+
+	public void AddCallBack (int frame, CallBack callBack) {
+		frame--;
+		if (frame >= frames.Count || frame < 0)
+		{
+			Debug.LogError ("The animation " + name + " does not contain the frame " + frame.ToString());
+			return;
+		}
+
+		if (callBack == null)
+		{
+			Debug.LogError ("A null callback is not allowed");
+			return;
+		}
+
+		if (callBacks.ContainsKey (frame))
+		{
+			callBacks [frame] += callBack;
+		}
+		else
+		{
+			callBacks.Add (frame, callBack);
+		}
+	}
+}
+
+public class Animator2D : MonoBehaviour {
+
+	public List<Animation> animations = new List<Animation>();
+
+	private SpriteRenderer spriteRenderer;
+
+	private Animation currentAnimation;
+	private Queue<Animation> animationQueue = new Queue<Animation>();
+	private Animation defaultAnimation;
+	private float timer;
+	private bool playing;
+
+
+	public void Restart () {
+		currentAnimation.Reset ();
+		playing = true;
+	}
+
+	public void Stop () {
+		currentAnimation.Reset ();
+		playing = false;
+	}
+		
+	public void Play () {
+		playing = true;
+	}
+
+	public void Pause () {
+		playing = false;
+	}
+
+	public void SetDefault (string name) {
+		Animation anim = FindAnimation (name);
+		if (anim == null)
+			return;
+
+		if (!anim.looping)
+		{
+			Debug.LogError ("The animation " + name + "is not a looping animation and thus not allowed as default");
+		}
+
+		defaultAnimation = anim;
+	}
+
+	public void DisableDefault () {
+		defaultAnimation = null;
+	}
+
+	public void AdvanceQueue () {
+		if (animationQueue.Count > 0)
+		{
+			Play (animationQueue.Dequeue (), true);
+		}
+	}
+
+	public void QueueAnimation (string name) {
+		Animation anim = FindAnimation (name);
+		if (anim == null)
+			return;
+
+		if (anim.looping)
+		{
+			Debug.LogError ("The queue only accepts non-looping animations. Set looping animations as default.");
+			return;
+		}
+
+		animationQueue.Enqueue (anim);
+	}
+
+	public void Switch (string name) {
+		Animation anim = FindAnimation (name);
+		if (anim == null)
+			return;
+
+		Play(anim, true);
+	}
+
+	public string CurrentAnimation () {
+		if (currentAnimation == null)
+		{
+			Debug.LogWarning ("No animation is currently playing");
+			return "";
+		}
+		else
+		{
+			return currentAnimation.name;
+		}
+	}
+
+	public bool IsPlaying () {
+		return playing;
+	}
+
+	public void AddCallBack (string animationName, int frame, CallBack callBack) {
+		Animation anim = FindAnimation (animationName);
+		if (anim == null)
+			return;
+		
+		anim.AddCallBack (frame, callBack);
+	}
+
+	private void Play (Animation animation, bool reset) {
+		if (reset)
+		{
+			animation.Reset ();
+		}
+
+		currentAnimation = animation;
+	}
+
+	private Animation FindAnimation (string name) {
+		Animation anim = null;
+		foreach (Animation a in animations)
+		{
+			if (a.name == name)
+			{
+				anim = a;
+			}
+		}
+
+		if (anim == null)
+		{
+			Debug.LogError ("The requested animation " + name + " was not found");
+			return null;
+		}
+
+		return anim;
+	}
+		
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () {
+
+		if (currentAnimation == null && defaultAnimation != null)
+		{
+			Play(defaultAnimation, true);
+		}
+
+		if (playing && currentAnimation != null)
+		{
+			if (timer > 0)
+			{
+				timer -= Time.deltaTime;
+			}
+			else
+			{
+				spriteRenderer.sprite = currentAnimation.GetFrame ();
+				currentAnimation.Increment ();
+
+				if (currentAnimation.ReachedEnd())
+				{
+					if (!currentAnimation.looping && animationQueue.Count > 0)
+					{
+						Debug.Log ("Queue");
+						Play(animationQueue.Dequeue(), true);
+					}
+					else if(currentAnimation != defaultAnimation && defaultAnimation != null)
+					{
+						Play(defaultAnimation, true);
+					}
+				}
+
+				timer = (float) (1.0f / currentAnimation.frameRate);
+			}
+		}
+	}
+}
---
+++ Assets/AnimatorSystem/ExampleScene/AnimatorUser.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorUser : MonoBehaviour {
+
+	public Animator2D anim2D;
+	public ParticleSystem partSys;
+	public AudioClip footstepSound;
+	public AudioClip punchSound;
+
+	private AudioSource audio;
+
+	private void Footstep () {
+		audio.PlayOneShot (footstepSound);
+	}
+
+	private void Punch () {
+		audio.PlayOneShot (punchSound);
+		partSys.Play ();
+	}
+		
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+
+		anim2D.AddCallBack ("Punch", 4, Punch);
+
+		anim2D.AddCallBack ("Run", 2, Footstep);
+		anim2D.AddCallBack ("Run", 6, Footstep);
+
+		anim2D.SetDefault ("Idle");
+		anim2D.Play ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			anim2D.Switch ("Punch");
+		}
+
+		if (Input.GetKeyDown (KeyCode.T))
+		{
+			anim2D.Switch ("Punch");
+			anim2D.QueueAnimation ("Punch");
+			anim2D.QueueAnimation ("Punch");
+		}
+
+		if (Input.GetKeyDown (KeyCode.P))
+		{
+			if (anim2D.IsPlaying ())
+			{
+				anim2D.Pause ();
+			}
+			else
+			{
+				anim2D.Play ();
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.D))
+		{
+			if(Input.GetKeyDown (KeyCode.A))
+				transform.localScale = new Vector3 (1, 1, 1);
+			else
+				transform.localScale = new Vector3 (-1, 1, 1);
+
+			anim2D.Switch ("Run");
+			anim2D.SetDefault ("Run");
+		}
+
+		if (Input.GetKeyDown (KeyCode.S))
+		{
+			anim2D.Switch ("Crouch");
+			anim2D.SetDefault ("CrouchIdle");
+		}
+
+		if (Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+		{
+			anim2D.Switch ("Stop");
+			anim2D.SetDefault ("Idle");
+			anim2D.AdvanceQueue ();
+		}
+
+		if (Input.GetKeyUp (KeyCode.S))
+		{
+			anim2D.Switch ("Idle");
+			anim2D.SetDefault ("Idle");
+		}
+	}
+}
